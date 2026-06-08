75aede8c30e4c4c3a9a1d874a31ec9fb3e3ad7f2
imboredlolll <baard.joshua@gmail.com>
2026-06-09 09:41:13 +1000

    Initial Commit

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-
+ 
 /**
  * This script is used to reset the project to a blank state.
  * It deletes or moves the /app, /components, /hooks, /scripts, and /constants directories to /app-example based on user input and creates a new /app directory with an index.tsx and _layout.tsx file.
