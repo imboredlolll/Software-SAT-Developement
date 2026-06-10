1e5f9c59355e2eef1e93af7fc8fefa3a52389c22
imboredlolll <baard.joshua@gmail.com>
2026-06-10 22:07:47 +1000

    Hard coded data

---
+++ constants/Todos.jsx
@@ -0,0 +1,34 @@
+export const Data = [
+    {
+        id: 1,
+        title: "Homework",
+        completed: false,
+        assignedBy: "teacher",
+        dueDateTime: "2026-07-01T17:00:00",
+        type: "School"
+    },
+    {
+        id: 2,
+        title: "Chores",
+        completed: false,
+        assignedBy: "parent",
+        dueDateTime: "2026-07-01T16:30:00",
+        type: "Home"
+    },
+    {
+        id: 3,
+        title: "Clean Room",
+        completed: false,
+        assignedBy: "teacher",
+        dueDateTime: "2026-06-01T00:00:00",
+        type: "Home"
+    },
+    {
+        id: 4,
+        title: "Homework",
+        completed: true,
+        assignedBy: "teacher",
+        dueDateTime: "2026-07-01T17:00:00",
+        type: "School"
+    }
+];
