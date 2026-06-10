2a42f0b8b6763fa8786a7a1073bbdd71960e9a74
imboredlolll <baard.joshua@gmail.com>
2026-06-10 22:06:38 +1000

    Put in proper colors and theme

---
+++ constants/Colors.js
@@ -0,0 +1,41 @@
+export const Colors = {
+  light: {
+    text: "#7ea37eb6",
+    secondaryText: "#8A958A",
+
+    background: "#F5F5F0",
+    card: "#FFFFFF",
+    cardBorder: "#E5E5E0",
+
+    icon: "#7EA37E",
+
+    dangerIcon: "#C27B67",      // overdue task accent
+    dangerText: "#72493f",
+    dangerBackground: "#FFF6F2",
+
+    progressTrack: "#E8E8E3",
+    progressFill: "#7EA37E",
+
+    navBar: "#FFFFFF",
+  },
+
+  dark: {
+    text: "#F3F5F7",
+    secondaryText: "#8E9AA8",
+
+    background: "#0F1724",
+    card: "#243142",
+    cardBorder: "#344355",
+
+    icon: "#8FB8D8",
+
+    dangerIcon: "#D28A72",
+    dangerText: "#f2c0af",
+    dangerBackground: "#4f3026",
+
+    progressTrack: "#344355",
+    progressFill: "#7EAAC8",
+
+    navBar: "#1A2432",
+  },
+};
