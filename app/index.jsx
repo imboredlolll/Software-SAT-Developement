1e6e8ace0a83813332306fd283306394481b715d
imboredlolll <baard.joshua@gmail.com>
2026-06-10 22:08:06 +1000

    Basic Front end for home page

---
+++ app/index.jsx
@@ -0,0 +1,240 @@
+import { Data } from "@/constants/Todos.jsx";
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { useContext, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemeContext } from "../context/ThemeContext";
+
+import { Quicksand_500Medium } from '@expo-google-fonts/quicksand/500Medium';
+import { useFonts } from '@expo-google-fonts/quicksand/useFonts';
+import { useWindowDimensions } from 'react-native';
+
+
+export default function Index() {
+
+  //Declare Todos
+  const [todos, setTodos] = useState(Data)
+
+  let todayTodos = []
+  let overdueTodos = []
+  let completedTodos = []
+
+  for (const todo of todos) {
+    if (todo.completed === false && new Date(todo.dueDateTime) > new Date()) {
+      todayTodos.push(todo)
+    }
+    else if (todo.completed === false && new Date(todo.dueDateTime) < new Date()) {
+      overdueTodos.push(todo)
+    }
+    else {
+      completedTodos.push(todo)
+    }
+  }
+
+  //Dynamic Sizes
+  const { width } = useWindowDimensions();
+  let fontSize = Math.max(20, width * 0.04);
+  let secondFontSize = Math.max(16, width * 0.03);
+  if (fontSize > 24) {
+    fontSize = 24
+  }
+  if (secondFontSize > 20) {
+    secondFontSize = 20
+  } 
+
+  //Declare colors + themes
+  const { colorScheme, setColorScheme ,theme } = useContext(ThemeContext)
+  const styles = createStyles(theme, colorScheme, fontSize, secondFontSize)
+
+  //Font
+  const [loaded, error] = useFonts(Quicksand_500Medium)
+
+  //Font loaded
+  if (!loaded && !error) {
+    return null
+  }
+
+  console.log("Hi")
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerTextContainer}>
+        <Text style={styles.helloText}>Good Morning Josh</Text>
+        <Text style={styles.bigText}>Your Tasks</Text>
+      </View>
+      <View style={styles.overdueTasksContainer}>
+        <Text style={styles.todosSubheaderText}>Overdue Tasks</Text>
+        <FlatList
+          data={overdueTodos}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={styles.contentContainer}
+          ListEmptyComponent={<Text>No items</Text>}
+          renderItem={({ item }) => (
+            <View style={{ ...styles.row, ...styles.overdueContainer}}>
+              <View style={styles.todoIconContainer}>
+                <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} style={styles.overdueCircleIcon}/>
+              </View>
+              <View style={styles.todoTextContainer}>
+                <Text style={{...styles.todoTitleText, ...styles.overdueTodoTitleText}} numberOfLines={1} adjustsFontSizeToFit>{item.title}</Text>
+                <Text style={{...styles.dueTimeText, ...styles.overdueTimeText}}>Overdue From {new Date(item.dueDateTime).toLocaleTimeString("en-AU", {hour: "numeric",minute: "2-digit"})}</Text>
+              </View>
+              <View style={styles.dotIconContainer}>
+                <Entypo name="dot-single" size={48} style={styles.overdueDot} />
+              </View>
+            </View>
+
+          )}
+        />
+      </View>
+      <View style={styles.dueTodayTasksContainer}>
+        <Text style={styles.todosSubheaderText}>Today</Text>
+        <FlatList
+          data={todayTodos}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={styles.contentContainer}
+          ListEmptyComponent={<Text>No items</Text>}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <View style={styles.todoIconContainer}>
+                <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} style={styles.circleIcon}/>
+              </View>
+              <View style={styles.todoTextContainer}>
+                <Text style={styles.todoTitleText} numberOfLines={1} adjustsFontSizeToFit>{item.title}</Text>
+                <Text style={styles.dueTimeText}>{new Date(item.dueDateTime).toLocaleTimeString("en-AU", {hour: "numeric",minute: "2-digit"})}</Text>
+              </View>
+              <View style={styles.dotIconContainer}>
+                <Entypo name="dot-single" size={48} style={styles.dot} />
+              </View>
+            </View>
+
+          )}
+        />
+      </View>
+      <View style={styles.completedTasksContainter}>
+        <Text style={styles.todosSubheaderText}>Completed</Text>
+        <FlatList
+          data={completedTodos}
+          keyExtractor={(item) => item.id.toString()}
+          contentContainerStyle={styles.contentContainer}
+          ListEmptyComponent={<Text>No items</Text>}
+          renderItem={({ item }) => (
+            <View style={styles.row}>
+              <View style={styles.todoIconContainer}>
+                <MaterialCommunityIcons name="checkbox-blank-circle-outline" size={24} style={styles.circleIcon}/>
+              </View>
+              <View style={styles.todoTextContainer}>
+                <Text style={{...styles.todoTitleText, ...styles.completedTodoText}} numberOfLines={1} adjustsFontSizeToFit>{item.title}</Text>
+                <Text style={styles.dueTimeText}>Done</Text>
+              </View>
+              <View style={styles.dotIconContainer}>
+                <Entypo name="dot-single" size={48} style={styles.doneDot} />
+              </View>
+            </View>
+
+          )}
+        />
+      </View>
+
+    </SafeAreaView>
+  );
+}
+
+
+function createStyles(theme, colorScheme, fontSize, secondFontSize) {
+  return StyleSheet.create({
+    container:{
+      flex: 1,
+      backgroundColor: theme.background
+    },
+    row: {
+      flexDirection: "row",
+      alignItems: "center",
+      width: "85%",
+      height: 100,
+      borderWidth: 1,
+      borderRadius: 20,
+      backgroundColor: theme.card,
+      marginHorizontal: 'auto'
+    },
+    todoIconContainer: {
+      width: 50,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    circleIcon: {
+      color: theme.icon
+    },
+    todoTextContainer: {
+      flex: 1,
+      justifyContent: "center",
+      paddingLeft: 10,
+    },
+    todoTitleText: {
+      paddingTop: "1%",
+      fontFamily: 'Quicksand_500Medium',
+      fontSize: fontSize,
+      color: theme.text
+    },
+    dueTimeText: {
+      paddingTop: 10,
+      fontFamily: 'Quicksand_500Medium',
+      fontSize: 22,
+      color: theme.secondaryText,
+      fontSize: secondFontSize,
+    },
+    dotIconContainer: {
+      width: 40,
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    dot: {
+      color: theme.icon,
+    },
+    overdueContainer: {
+      backgroundColor: theme.dangerBackground,
+      borderColor: theme.dangerIcon
+    },
+    overdueCircleIcon: {
+      color: theme.dangerIcon
+    },
+    overdueDot:{
+      color: theme.dangerIcon
+    },
+    overdueTodoTitleText:{
+      color: theme.dangerText
+    },
+    overdueTimeText:{
+      color: theme.dangerText
+    },
+    completedTodoText: {
+      textDecorationLine: "line-through",
+      color: theme.secondaryText
+    },
+    doneDot:{
+      color: theme.card
+    },
+    helloText: {
+      color: theme.secondaryText,
+      fontSize: 22,
+      paddingTop: 15,
+      fontFamily: 'Quicksand_500Medium',
+      paddingLeft: "8%"
+    },
+    bigText: {
+      color: theme.text,
+      fontSize: 34,
+      paddingTop: 6,
+      fontFamily: 'Quicksand_500Medium',
+      paddingLeft: "8%"
+    },
+    todosSubheaderText: {
+      color: theme.secondaryText,
+      fontSize: 22,
+      paddingTop: 10,
+      paddingBottom: 10,
+      fontFamily: 'Quicksand_500Medium',
+      paddingLeft: "8%"
+    },
+  })
+}
