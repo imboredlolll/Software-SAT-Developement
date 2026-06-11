8624cd0567b37aadecfc81e06b5fa4ac563c43fc
imboredlolll <baard.joshua@gmail.com>
2026-06-11 14:52:37 +1000

    Added in a bit more hardcoded data

--- constants/Todos.jsx
+++ constants/Todos.jsx
@@ -30,5 +30,53 @@ export const Data = [
         assignedBy: "teacher",
         dueDateTime: "2026-07-01T17:00:00",
         type: "School"
+    },
+    {
+        id: 5,
+        title: "Math Assignment",
+        completed: false,
+        assignedBy: "teacher",
+        dueDateTime: "2026-07-01T19:00:00",
+        type: "School"
+    },
+    {
+        id: 6,
+        title: "Take Out Bins",
+        completed: false,
+        assignedBy: "parent",
+        dueDateTime: "2026-07-01T18:00:00",
+        type: "Home"
+    },
+    {
+        id: 7,
+        title: "Study for Science Test",
+        completed: true,
+        assignedBy: "teacher",
+        dueDateTime: "2026-07-01T20:30:00",
+        type: "School"
+    },
+    {
+        id: 8,
+        title: "Football Training",
+        completed: false,
+        assignedBy: "self",
+        dueDateTime: "2026-07-01T17:30:00",
+        type: "Personal"
+    },
+    {
+        id: 9,
+        title: "English Essay",
+        completed: false,
+        assignedBy: "teacher",
+        dueDateTime: "2026-06-01T15:00:00",
+        type: "School"
+    },
+    {
+        id: 10,
+        title: "Wash Dishes",
+        completed: false,
+        assignedBy: "parent",
+        dueDateTime: "2026-06-05T18:00:00",
+        type: "Home"
     }
 ];
